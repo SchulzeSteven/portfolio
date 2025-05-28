02409537f44bd8e98046eee91a8ffc9c38ee180e
Steven Schulze <schulze89@hotmail.de>
2025-05-28 18:14:52 +0200

    added first imports

--- src/app/main-content/main-content.component.ts
+++ src/app/main-content/main-content.component.ts
@@ -1,9 +1,15 @@
 import { Component } from '@angular/core';
+import { LandingPageComponent } from '../landing-page/landing-page.component';
+import { AboutMeComponent } from '../about-me/about-me.component';
+import { SkillsComponent } from '../skills/skills.component';
+import { FeaturedProjectsComponent } from '../featured-projects/featured-projects.component';
+import { ReferencesComponent } from '../references/references.component';
+import { ContactMeComponent } from '../contact-me/contact-me.component';
 
 @Component({
   selector: 'app-main-content',
   standalone: true,
-  imports: [],
+  imports: [LandingPageComponent, AboutMeComponent, SkillsComponent, FeaturedProjectsComponent, ReferencesComponent, ContactMeComponent],
   templateUrl: './main-content.component.html',
   styleUrl: './main-content.component.scss'
 })
