836178cd39a2dba13c8de4563b58cdfe8d0fb61c
Steven Schulze <schulze89@hotmail.de>
2025-07-09 04:12:27 +0200

    fixed margin landing-page and added Language Button

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,12 +1,17 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss'
 })
 export class NavbarComponent {
+  selectedLanguage: 'EN' | 'DE' = 'EN';
 
+  toggleLanguage() {
+    this.selectedLanguage = this.selectedLanguage === 'EN' ? 'DE' : 'EN';
+  }
 }
