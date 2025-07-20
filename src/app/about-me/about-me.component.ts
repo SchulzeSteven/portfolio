4bcf45e8688c3e7c52a4bbfaa8a6afee8dd57c0a
Steven Schulze <schulze89@hotmail.de>
2025-07-20 22:36:13 +0200

    added about me section and text incl. image

--- src/app/about-me/about-me.component.ts
+++ src/app/about-me/about-me.component.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-about-me',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, TranslateModule], // <-- CommonModule hinzufügen
   templateUrl: './about-me.component.html',
   styleUrl: './about-me.component.scss'
 })
 export class AboutMeComponent {
+  hasMoved = false;
 
+  onHover(): void {
+    this.hasMoved = true;
+  }
 }
