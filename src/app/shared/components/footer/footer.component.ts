b47efe3c1a33e611c5749e2ae7219b6a88c4ce2f
Steven Schulze <schulze89@hotmail.de>
2025-07-28 16:37:57 +0200

    added footer and added new email

--- src/app/shared/components/footer/footer.component.ts
+++ src/app/shared/components/footer/footer.component.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { TranslatePipe } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [],
+  imports: [TranslatePipe, RouterLink],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.scss'
 })
 export class FooterComponent {
-
+  backToBegin() {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+  }
 }
