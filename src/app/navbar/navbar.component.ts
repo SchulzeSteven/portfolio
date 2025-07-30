7df73f1b8d31224fa3de1e116969e283136c8e55
Steven Schulze <schulze89@hotmail.de>
2025-07-30 19:18:52 +0200

    added icons and fixed responsive navbar

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Renderer2 } from '@angular/core'; // Renderer2 hinzufügen!
 import { CommonModule } from '@angular/common';
 import { TranslateService, TranslateModule } from '@ngx-translate/core';
 
@@ -11,14 +11,39 @@ import { TranslateService, TranslateModule } from '@ngx-translate/core';
 })
 export class NavbarComponent {
   selectedLanguage: 'EN' | 'DE' = 'EN';
+  menuIsClose = true;
+  dialogVisible = true;
+  isIconInverted = false;
 
-  constructor(public translate: TranslateService) {
+  constructor(public translate: TranslateService, private renderer: Renderer2) {
     const savedLang = localStorage.getItem('lang');
     const defaultLang = savedLang ?? 'en';
 
     this.translate.use(defaultLang);
     this.selectedLanguage = defaultLang.toUpperCase() === 'DE' ? 'DE' : 'EN';
   }
+
+  toggleIconFilter() {
+  this.isIconInverted = !this.isIconInverted;
+}
+
+  switchMenu(): void {
+    this.menuIsClose = !this.menuIsClose;
+
+    if (!this.menuIsClose) {
+      this.renderer.setStyle(document.body, 'overflow', 'hidden');
+    } else {
+      this.renderer.removeStyle(document.body, 'overflow');
+    }
+  }
+
+  scrollToSection(id: string) {
+  const element = document.getElementById(id);
+  if (element) {
+    element.scrollIntoView({ behavior: 'smooth' });
+    this.switchMenu();
+  }
+}
 
   toggleLanguage() {
     this.selectedLanguage = this.selectedLanguage === 'EN' ? 'DE' : 'EN';
